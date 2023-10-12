c2290d47cc8b30e2d534070650e44a681ec2563e
HungNP <21520252@gm.uit.edu.vn>
2023-10-12 20:50:16 +0700

    useFactory to config typeorm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,7 @@ import { UploadModule } from "./upload/upload.module";
 import { ConfigModule } from "@nestjs/config";
 import { AccountModule } from "./account/account.module";
 import { JwtModule } from "@nestjs/jwt";
+import { createClient } from "redis";
 
 @Module({
     imports: [
@@ -17,25 +18,49 @@ import { JwtModule } from "@nestjs/jwt";
             secret: process.env.ACCESS_TOKEN_SECRET,
             global: true,
         }),
-        TypeOrmModule.forRoot({
-            type: "postgres",
-            url:
-                process.env.IS_PRODUCTION == "false"
-                    ? "postgres://postgres@localhost:5432/db"
-                    : process.env.DB_URL,
-            synchronize: true,
-            entities: ["dist/**/*.entity{.ts,.js}"],
-            cache: {
-                type: "redis",
-                ignoreErrors: true,
-                options:
-                    process.env.IS_PRODUCTION == "false"
-                        ? {
-                              url: "redis://localhost:6379",
-                          }
-                        : {
-                              url: process.env.REDIS_URL,
-                          },
+        TypeOrmModule.forRootAsync({
+            useFactory: async () => {
+                if (process.env.IS_PRODUCTION == "true") {
+                    return {
+                        type: "postgres",
+                        url: process.env.DB_URL,
+                        synchronize: false,
+                        entities: ["dist/**/*.entity{.ts,.js}"],
+                        cache: {
+                            duration: 5000,
+                            type: "redis",
+                            ignoreErrors: true,
+                            options: {
+                                url: process.env.REDIS_URL,
+                            },
+                        },
+                    };
+                } else {
+                    const client = createClient();
+
+                    client.on("error", (error) => {
+                        console.error(error);
+                    });
+                    client.on("connect", async () => {
+                        console.log("Connected to redis");
+                    });
+                    await client.connect();
+                    await client.disconnect();
+                    return {
+                        type: "postgres",
+                        url: "postgres://postgres@localhost:5432/db",
+                        synchronize: true,
+                        entities: ["dist/**/*.entity{.ts,.js}"],
+                        duration: 5000,
+                        cache: {
+                            type: "redis",
+                            ignoreErrors: true,
+                            options: {
+                                url: "redis://localhost:6379",
+                            },
+                        },
+                    };
+                }
             },
         }),
         AuthModule,

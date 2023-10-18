a52e2ee5a70fa0bdb47c4ee75a6115855a662efc
HungNP <21520252@gm.uit.edu.vn>
2023-10-18 19:20:10 +0700

    add javadoc

--- src/hash/hash.service.ts
+++ src/hash/hash.service.ts
@@ -3,7 +3,17 @@ import { compareSync, hashSync } from "bcrypt";
 import { hashRounds } from "../helper/constant";
 
 export abstract class HashService {
+    /**
+     * hash a string value
+     * @param value value to be hashed
+     */
     abstract hash(value: string): string;
+    
+    /**
+     * compare a string value with a hashed string
+     * @param value value to be compared
+     * @param hash hashed string value
+     */
     abstract isMatch(value: string, hash: string): boolean;
 }
 

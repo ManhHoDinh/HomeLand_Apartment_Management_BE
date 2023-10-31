e2427c99a2d9b8533611bbe9e04e6bf8a27966c9
HungNP <21520252@gm.uit.edu.vn>
2023-10-31 19:46:37 +0700

    remove person-related fiiles

--- src/person/dto/update-person.dto.ts
+++
@@ -1,9 +0,0 @@
-import { OmitType, PartialType } from "@nestjs/swagger";
-import { CreatePersonDto } from "./create-person.dto";
-
-export class UpdatePersonDto extends PartialType(
-    OmitType(CreatePersonDto, [
-        "back_identify_card_photo",
-        "front_identify_card_photo",
-    ] as const),
-) {}
